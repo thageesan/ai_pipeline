31442ad8ce3ccce544969a2a6414a0e6ea6c6265
thageesan <thageesan@gmail.com>
2021-08-10 18:45:09 +0000

    dvc: commit experiment 44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a

--- params.py
+++ params.py
@@ -9,7 +9,7 @@ class TestTrainSetConfig:
 
 class XGBTrainConfig:
     subsample = 0.8
-    n_estimators = 500
+    n_estimators = 400
     min_child_weight = 5
     max_depth = 6
     learning_rate = 0.1
